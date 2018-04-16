320a5707514b085b190716055974312f7e6e22c3
rkekre <rahul.kekre@oracle.com>
2018-04-16 11:40:35 -0700

    test script

---
+++ serve.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+from SocketServer import TCPServer, StreamRequestHandler
+import socket
+import logging
+import datetime
+
+
+class Handler(StreamRequestHandler):
+    def handle(self):
+        self.data = self.rfile.readline().strip()
+        logging.info("From <%s>: %s" % (self.client_address, self.data))
+        self.wfile.write("Message: " + self.data.upper() + "\tCurrent Time: " + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S")  + "\n")
+
+class Server(TCPServer):
+    
+    # The constant would be better initialized by a systemd module
+    SYSTEMD_FIRST_SOCKET_FD = 3
+
+    def __init__(self, server_address, handler_cls):
+        # Invoke base but omit bind/listen steps (performed by systemd activation!)
+        TCPServer.__init__(
+            self, server_address, handler_cls, bind_and_activate=False)
+        # Override socket
+        self.socket = socket.fromfd(
+            self.SYSTEMD_FIRST_SOCKET_FD, self.address_family, self.socket_type)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    HOST, PORT = "localhost", 9999 # not really needed here
+    server = Server((HOST, PORT), Handler)
+    server.serve_forever()
---
+++ time-now.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+import time
+import datetime
+ 
+while True:
+    print "Current date and time: " , datetime.datetime.now()
+    time.sleep(10)
